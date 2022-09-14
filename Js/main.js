4fe669d456167329ab0ceb851a2d347d4b9e980c
unknown <medo39286@gmail.com>
2022-09-15 01:37:21 +0200

    creating about us page

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,24 @@
 let nav = document.querySelector(".nav-bar");
 let arrow = document.querySelector(".nav-bar .arrow");
 let taps = document.querySelectorAll(".nav-bar h3")
+let icons = document.querySelectorAll("svg:not(.arr)")
 // let nav = document.querySelector('.nav-bar')
 // let nav = document.querySelector('.nav-bar')
-
+// create arrow button in nav-bar
 arrow.onclick = () => {
   nav.classList.toggle("active");
+  taps.forEach(ele => {
+    if(nav.classList.contains("active")=== true  ){
+      ele.style.display ="none";
+       icons.forEach((ico) => {
+        ico.style.marginBottom = "20px"
+       });
+    }else {
+      ele.style.display ="block"
+    }
+  });
+ 
   
 };
 
-// how to loop by forEach ?
+
