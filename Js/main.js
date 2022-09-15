91ce18104dde1f873cf55f8cbed89970f9b722a9
unknown <medo39286@gmail.com>
2022-09-15 13:33:03 +0200

    edit js

--- Js/main.js
+++ Js/main.js
@@ -2,6 +2,8 @@ let nav = document.querySelector(".nav-bar");
 let arrow = document.querySelector(".nav-bar .arrow");
 let taps = document.querySelectorAll(".nav-bar h3")
 let icons = document.querySelectorAll("svg:not(.arr)")
+let over = document.querySelector(".over");
+let remin = document.querySelector(".reminder")
 // let nav = document.querySelector('.nav-bar')
 // let nav = document.querySelector('.nav-bar')
 // create arrow button in nav-bar
@@ -20,5 +22,13 @@ arrow.onclick = () => {
  
   
 };
+let rem = setTimeout(function(){
+over.style.display="block";
+remin.style.display="flex";
+over.onclick =function(){
+  over.style.display = "none";
+  remin.style.display = "none";
+}
+}, 5000)
 
 
