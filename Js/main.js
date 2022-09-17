abfa190dc6a5a693dd700d37176c42a07b39f4d3
YASOOCODE <yaseensaadeldinmohamed@gmail.com>
2022-09-17 11:30:52 +0200

    Nav Eddited

--- Js/main.js
+++ Js/main.js
@@ -3,9 +3,6 @@ let arrow = document.querySelector(".nav-bar .arrow");
 let taps = document.querySelectorAll(".nav-bar h3");
 let icons = document.querySelectorAll("svg:not(.arr)");
 let over = document.querySelector(".over");
-<<<<<<< HEAD
-let remin = document.querySelector(".reminder")
-=======
 let remin = document.querySelector(".reminder");
 let quesBoxes = document.querySelectorAll(".ques-box");
 console.log(quesBoxes);
@@ -17,33 +14,11 @@ let smell = document.querySelector(".smell-box");
 console.log(anses);
 // let nav = document.querySelector('.nav-bar')
 // let nav = document.querySelector('.nav-bar')
->>>>>>> 0b2f60ff1b07c90def335cfc8fe80b700998604c
 // create arrow button in nav-bar
 arrow.onclick = () => {
   nav.classList.toggle("active");
-  taps.forEach((ele) => {
-    if (nav.classList.contains("active") === true) {
-      ele.style.display = "none";
-      icons.forEach((ico) => {
-        ico.style.marginBottom = "20px";
-      });
-    } else {
-      ele.style.display = "block";
-    }
-  });
 };
-<<<<<<< HEAD
 
-let rem = setTimeout(function(){
-over.style.display="block";
-remin.style.display="flex";
-over.onclick =function(){
-  over.style.display = "none";
-  remin.style.display = "none";
-}
-}, 8000)
-
-=======
 let rem = setTimeout(function () {
   over.style.display = "block";
   remin.style.display = "flex";
@@ -51,7 +26,7 @@ let rem = setTimeout(function () {
     over.style.display = "none";
     remin.style.display = "none";
   };
-}, 5000);
+}, 8000);
 
 // riArr.forEach(aro => {
 //   aro.onclick = function(){
@@ -68,16 +43,16 @@ let rem = setTimeout(function () {
 for (let i = 0; i < riArr.length; i++) {
   riArr[i].onclick = function () {
     quesBoxes[i].style.display = "none";
-    if (quesBoxes[i].classList.contains("smell-box") === true){
-      anses.style.display ="grid"
-    } console.log(quesBoxes[i]);
+    if (quesBoxes[i].classList.contains("smell-box") === true) {
+      anses.style.display = "grid";
+    }
+    console.log(quesBoxes[i]);
     quesBoxes[i + 1].style.display = "flex";
     // if(riArr.slice(-1).pop() === riArr[i]){
 
     // }
   };
 }
->>>>>>> 0b2f60ff1b07c90def335cfc8fe80b700998604c
 
 // let aft = window.getComputedStyle(document.querySelector(".ques-box"), ":after");
 // aft.onclick =function(){
